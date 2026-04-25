bbbe7cd79b64dc79f2de0c2ee789ddb915ee6418
maxrgbg <maxrg@max-notebook.localdomain>
2026-04-25 23:42:48 +0200

    ui

--- ui/projects/shared/src/lib/shared.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'lib-shared',
-  imports: [],
-  template: ` <p>shared works!</p> `,
-  styles: ``,
-})
-export class Shared {}
--- ui/projects/shared/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of shared
- */
-
-export * from './lib/navbar/navbar';
-export * from './lib/sitebar/sitebar';
